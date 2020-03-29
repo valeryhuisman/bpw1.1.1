e3d75585244e852873fbd6ed26d5dd34a699eb6d
valeryhuisman <valeryhuisman@gmail.com>
2020-03-29 21:41:23 +0200

    bpw forest game 1.1.1 (cursor fixed)

---
+++ game2/Assets/scripts/CursorVisibility.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour
+{
+    void OnLevelWasLoaded(int level)
+    {
+            Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        //Debug.Log("This is working, bot not the cursor");
+        
+    }
+}
